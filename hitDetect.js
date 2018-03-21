db6f6819e61d3c649569a73cdf8440e751283cab
zhoudaxia2016 <1207190489@qq.com>
2018-03-21 23:28:27 +0800

    将collada loader改为json loader，出现动画加载模型变形问题

--- hitDetect.js
+++ hitDetect.js
@@ -16,3 +16,29 @@ function groundHitDetect (obj, land) {
     return new THREE.Vector3(0, adjust, 0)
   }
 }
+
+// Create bounding box
+function AABB (obj) {
+  let xmax = Number.NEGATIVE_INFINITY, xmin = Number.POSITIVE_INFINITY
+  let ymax = xmax, ymin = xmin
+  console.log(obj)
+  let vts = obj.geometry.vertices
+  for (let i = 0, l = vts.length; i < l; i++) {
+    let { x, y } = vts[i]
+    xmax = Math.max(xmax, x), ymax = Math.max(ymax, y)
+    xmin = Math.min(xmin, x), ymin = Math.min(ymin, y)
+  }
+  return { xmax, xmin, ymax, ymin }
+}
+
+function bbox (obj) {
+  let geometry = new THREE.BoxHelper(obj).geometry
+  geometry.computeBoundingBox()
+  let box = geometry.boundingBox
+  return {
+    min: { x: box.min.x, z: box.min.z },
+    max: { x: box.max.x, z: box.max.z }
+  }
+}
+
+
